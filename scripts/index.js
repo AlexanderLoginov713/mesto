cee929ad1d0e1a1c99f13e17dace78af82284b13
AlexanderLoginov713 <95143793+AlexanderLoginov713@users.noreply.github.com>
2022-03-23 19:44:44 +0300

    hotfix/elements-height

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,19 @@
 const popupElement = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popupElement.querySelector('.popup__close-button');
-const ESC_KEY = "Escape";
+//const ESC_KEY = "Escape";
 const formElement = document.querySelector('.popup__form');
 const nameInput = formElement.querySelector('input[name=name]');
 const jobInput = formElement.querySelector('input[name=job]');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-const likeButtons = document.querySelectorAll('.element__like-btn');
+//const likeButtons = document.querySelectorAll('.element__like-btn');
 
-for (let likeButton of likeButtons) {
+/*for (let likeButton of likeButtons) {
     likeButton.onclick = function () {
     likeButton.classList.toggle('element__like-btn_active');
   }
-}
+}*///
 
 function openPopup () {
   popupElement.classList.add('popup_opened');
@@ -27,11 +27,11 @@ function closePopup () {
   document.removeEventListener('keyup', onDocumentKeyUp);
 }
 
-function onDocumentKeyUp(event) {
+/* function onDocumentKeyUp(event) {
    if (event.key === ESC_KEY) {
      closePopup();
    }
-  }
+  }*/
 
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
